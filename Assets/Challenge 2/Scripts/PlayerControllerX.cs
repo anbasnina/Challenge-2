c98f67009c620a7c3e685f0cda19db39fce628fc
anastasiagame <anastasia.icloudgold@gmail.com>
2021-08-16 09:28:40 +0300

    unit 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private bool pressed = true;
+    private float delayShoot = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && pressed)
+        {
+            pressed = false;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Invoke("waitingShooting", delayShoot);
+        }
+    }
+
+    void waitingShooting()
+    {
+        pressed = true;
+    }
+}
